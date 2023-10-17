761cb0de68a757da03b9d7452edaee973adaf4bc
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2023-10-17 16:08:28 -0500

    Insert trial code to change color of grid to black

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const gridContainer = document.querySelector('#grid-container');
 //CREATE 16x16 grid
-let n = 16;
-for (let i = 0; i<n; i++) {
-    const rowDiv = document.createElement('div');
-    rowDiv.setAttribute('class', 'row-div');
-    for (let j = 0; j<n; j++) {
-        const squareDiv = document.createElement('div');
-        squareDiv.setAttribute('class', 'square-div');
-        rowDiv.appendChild(squareDiv);}
-    gridContainer.appendChild(rowDiv);
+function createGrid(n) {
+    for (let i = 0; i<n; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.setAttribute('class', 'row-div');
+        for (let j = 0; j<n; j++) {
+            const squareDiv = document.createElement('div');
+            squareDiv.setAttribute('class', 'square-div');
+            squareDiv.addEventListener('mouseover', colorChange);
+            rowDiv.appendChild(squareDiv);}
+        gridContainer.appendChild(rowDiv);
+    }
 }
+
+function colorChange(e) {
+    e.target.style.backgroundColor = 'black';
+}
+
+createGrid(16);
