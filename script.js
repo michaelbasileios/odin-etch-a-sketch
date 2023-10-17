508f4aabf61c614fc2232368bf58f5d9c61c69f3
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2023-10-17 11:43:04 -0500

    Create basic square div in js

---
+++ script.js
@@ -0,0 +1,5 @@
+//GRID SQUARE UNIT
+const squareDiv = document.createElement('div');
+squareDiv.setAttribute('id', 'square-div');
+
+document.querySelector('#grid-container').appendChild(squareDiv);
