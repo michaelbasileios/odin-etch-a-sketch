d167769acb244d11636443c55ad301100811b283
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2023-10-17 16:25:09 -0500

    Add code to randomize color of grid squares on mouseover

--- script.js
+++ script.js
@@ -14,7 +14,11 @@ function createGrid(n) {
 }
 
 function colorChange(e) {
-    e.target.style.backgroundColor = 'black';
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let color = `rgb(${r}, ${g}, ${b})`;
+    e.target.style.backgroundColor = color;
 }
 
 createGrid(16);
