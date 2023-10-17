7b680459713c4c4359f2c36db8684aaa3e7d836c
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2023-10-17 15:07:14 -0500

    Add js code to create grid based on a given int

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-//GRID SQUARE UNIT
-const squareDiv = document.createElement('div');
-squareDiv.setAttribute('id', 'square-div');
-
-document.querySelector('#grid-container').appendChild(squareDiv);
+const gridContainer = document.querySelector('#grid-container');
+//CREATE 16x16 grid
+let n = 16;
+for (let i = 0; i<n; i++) {
+    const rowDiv = document.createElement('div');
+    rowDiv.setAttribute('class', 'row-div');
+    for (let j = 0; j<n; j++) {
+        const squareDiv = document.createElement('div');
+        squareDiv.setAttribute('class', 'square-div');
+        rowDiv.appendChild(squareDiv);}
+    gridContainer.appendChild(rowDiv);
+}
